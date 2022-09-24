6ab4126ef68ea46b6fb70c7723b07161f6bc93e0
Batuhan Derya <bderya@3-G-9.42heilbronn.de>
2022-09-24 20:01:27 +0200

    FirstTest

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
-		if (500 < from1UpTo1000)
+		if (500 > from1UpTo1000)
 		{
 			System.out.println("Eagle");
 		}
